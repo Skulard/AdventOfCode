bf38ab45afd98a723003934a40d3efd9ce481762
Kisling <kisling@kirchheim.bickhardt-bau.de>
2025-12-03 16:56:42 +0100

    Day one and Day Two added. Sadly part two of day two is only in the beginning sages
    Adjusted commits

---
+++ 2025/DayOne.cs
@@ -0,0 +1,67 @@
+﻿namespace _2025;
+
+public class DayOne
+{
+    AllFiles allFiles = new();
+    public DayOne()
+    {
+        Console.WriteLine("Welcome to hell of Day ONE");
+        PartOne();
+        PartTwo();
+    }
+
+    void PartOne()
+    {
+        int dial = 50;
+        int counter = 0;
+        var input = allFiles.DayOne.Split("\n");
+        Console.WriteLine("This one should only count when it hits 0");
+
+        //a foreach with switch with counting
+        foreach (var line in input)
+        {
+            char direction = line.ToCharArray()[0];
+            switch (direction)
+            {
+                case 'L':
+                    dial -= Convert.ToInt32(line.Substring(1));
+                    break;
+                case 'R':
+                    dial += Convert.ToInt32(line.Substring(1));
+                    break;
+            }
+            if (dial%100 == 0) counter++;
+        }
+        Console.WriteLine($"It hit {counter} times");
+    }
+    void PartTwo()
+    {
+        int dial = 50;
+        int counter = 0;
+        var input = allFiles.DayOne.Split('\n');
+        Console.WriteLine("This one should count when it goes beyond 0 and when it hits 0");
+        bool startedAtZero = false;
+        //like part one. but this time with divide 100 to count every variation and then ?
+        foreach (var line in input)
+        {
+            char direction = line.ToCharArray()[0];
+            switch (direction)
+            {
+                case 'L':
+                    dial -= Convert.ToInt32(line.Substring(1));
+                    break;
+                case 'R':
+                    dial += Convert.ToInt32(line.Substring(1));
+                    break;
+            }
+            counter += Convert.ToInt32(Math.Floor(Convert.ToDouble(Math.Abs(dial) / 100)));
+            if(dial < 0 && !startedAtZero)counter++;
+            if (dial == 0) counter++;
+
+            dial = dial % 100;
+            if (dial == 0) startedAtZero = true; else startedAtZero= false;
+            if (dial < 0) dial += 100;
+        }
+        Console.WriteLine($"It counts: {counter}");
+    }
+}
---
+++ 2025/DayTwo.cs
@@ -0,0 +1,72 @@
+﻿namespace _2025;
+
+public class DayTwo
+{
+    AllFiles allFiles = new();
+    public DayTwo()
+    {
+        Console.WriteLine("Welcome to Hell Day Two");
+        PartOne();
+        PartTwo();
+    }
+
+    //44854383294
+    void PartOne()
+    {
+        long total = 0;
+        var read1 = allFiles.DayTwo.Split(',');
+        foreach (var item in read1)
+        {
+            var range = item.Split("-");
+            long begin = Convert.ToInt64(range[0]);
+            long end = Convert.ToInt64(range[1]);
+            string endString = end.ToString();
+            string beginString = begin.ToString();
+            for (; begin <= end; begin++)
+            {
+                beginString = begin.ToString();
+
+                if ((beginString.Length % 2 == 1) && (endString.Length % 2 == 1))
+                    break;
+                
+                int stringHalf = beginString.Length / 2;
+                
+                if (beginString.Length > 1 && beginString.Length % 2 == 0)
+                {
+                    long part1 = Convert.ToInt64(beginString.Substring(0,stringHalf));
+                    long part2 = Convert.ToInt64(beginString.Substring(beginString.Length - stringHalf, stringHalf));
+
+                    if (part1 == part2) total += begin;
+                }
+
+            }
+        }
+        Console.WriteLine($"Solution is: {total}");
+    }
+    void PartTwo()
+    {
+        //ID is invalid if sequence of digits repeated at least twice
+        long count = 0;
+        List<string> ranges=allFiles.DayTwo.Split(",").ToList();
+        foreach(var range in ranges)
+        {
+            List<string> items = range.Split("-").ToList();
+            long begin = Convert.ToInt64(items[0]);
+            long end = Convert.ToInt64(items[1]);
+            string beginstring = begin.ToString();
+            string endstring = end.ToString();
+            for(; begin <= end; begin++)
+            {
+                beginstring = begin.ToString();
+                var beginchar = beginstring.ToCharArray();
+                
+                if(beginstring.Length > 1)
+                {
+                    long a = Convert.ToInt64(beginstring.Substring(0, beginstring.Length / 2));
+                    long b = Convert.ToInt64(beginstring.Substring((beginstring.Length - (beginstring.Length / 2)), (beginstring.Length / 2)));
+                    
+                }
+            }
+        }
+    }
+}
